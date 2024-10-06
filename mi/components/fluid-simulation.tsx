6aa03d662d299cfd7efd461995196c5af248dc9b
manfromexistence <ajju40959@gmail.com>
2024-10-06 13:32:26 +0000

    feat: files organized

--- mi/components/fluid-simulation.tsx
+++ mi/components/fluid-simulation.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export default function MyApp() {
     return (
         <>
-            <canvas className='absolute left-0 top-0 h-screen w-full z-10' />
+            <canvas className='absolute left-0 top-0 z-10 h-screen w-full' />
             <Script id="idk">
                 {`
                 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
