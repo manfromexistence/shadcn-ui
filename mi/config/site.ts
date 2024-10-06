cccd41da3ae5bb70124f2317cdd1465b9769bd1c
manfromexistence <ajju40959@gmail.com>
2024-10-06 12:46:08 +0000

    feat: will add fuilds in landing page

--- mi/config/site.ts
+++ mi/config/site.ts
@@ -1,23 +1,14 @@
 export const siteConfig = {
-  name: "Magic UI",
-  url: "https://magicui.design",
-  ogImage: "https://magicui.design/og",
+  name: "manfromexistence/ui",
+  url: "https://ui.manfromexistence.vercel.app",
+  ogImage: "https://ui.manfromexistence.vercel.app/og.jpg",
   description:
-    "Beautifully designed landing page components built with React & Tailwind CSS.",
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
   links: {
-    twitter: "https://twitter.com/magicuidesign",
-    discord: "https://discord.gg/87p2vpsat5",
-    github: "https://github.com/magicuidesign/magicui",
-    instagram: "https://instagram.com/magicuidesign/",
+    shadcn: "https://twitter.com/shadcn",
+    twitter: "https://twitter.com/manfromexistence",
+    github: "https://github.com/manfromexistence/ui",
   },
-  keywords: [
-    "React",
-    "Tailwind CSS",
-    "Framer Motion",
-    "Landing Page",
-    "Components",
-    "Next.js",
-  ],
-};
+}
 
-export type SiteConfig = typeof siteConfig;
+export type SiteConfig = typeof siteConfig
