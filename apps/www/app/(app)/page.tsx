5e210c7eac0e00318dcd035719d9e64efe17fac7
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-13 16:15:00 +0000

    feat: making actions for varients page

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -1,65 +1,44 @@
-import Image from "next/image"
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { Announcement } from "@/components/announcement"
-import { ExamplesNav } from "@/components/examples-nav"
-import {
-  PageActions,
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "@/components/page-header"
-import CardsNewYork from "@/registry/new-york/example/cards"
-import { Button } from "@/registry/new-york/ui/button"
+import { Announcement } from "@/components/announcement";
+import { PageActions, PageHeader, PageHeaderDescription, PageHeaderHeading } from "@/components/page-header";
+import { Button } from "@/registry/default/ui/button";
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
-    <div className="relative">
-      <PageHeader>
-        <Announcement />
-        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+    <div className="relative flex min-h-[500px] items-center justify-center">
+      {/* <h1 className="">Hello manfromexistence/ui</h1> */}
+      <PageHeader className="border-none">
+        {/* <Announcement /> */}
+        <PageHeaderHeading className="text-center ">Explore Varients</PageHeaderHeading>
         <PageHeaderDescription>
-          Beautifully designed components that you can copy and paste into your
-          apps. Made with Tailwind CSS. Open source.
+          See some varients. Copy and paste into your apps. Open Source.
         </PageHeaderDescription>
-        <PageActions>
+        <section className="mx-auto mt-4 h-[250px] w-full space-y-1">
+          <div className="top flex items-center justify-center space-x-1">
+            <div className="flex h-[125px] w-full flex-col items-center justify-center space-y-1 rounded-tl-lg border text-primary/70 hover:bg-primary-foreground hover:text-primary">
+              <h3>Power Modes</h3>
+              <span className="flex h-8 w-8 items-center justify-center rounded-full border">3</span>
+            </div>
+            <div className="flex h-[125px] w-full flex-col items-center justify-center space-y-1 rounded-tr-lg border text-primary/70 hover:bg-primary-foreground hover:text-primary">
+              <h3>Fluids</h3>
+              <span className="flex h-8 w-8 items-center justify-center rounded-full border">10</span>
+            </div>
+          </div>
+          <div className="flex h-[125px] w-full flex-col items-center justify-center space-y-1 rounded-b-lg border text-primary/70 hover:bg-primary-foreground hover:text-primary">
+            <h3>Switchers</h3>
+            <span className="flex h-8 w-8 items-center justify-center rounded-full border">13</span>
+          </div>
+        </section>
+
+        {/* <PageActions>
           <Button asChild size="sm">
-            <Link href="/docs">Get Started</Link>
+            <a href="#charts">Browse Charts</a>
           </Button>
-          <Button asChild size="sm" variant="ghost">
-            <Link
-              target="_blank"
-              rel="noreferrer"
-              href={siteConfig.links.github}
-            >
-              GitHub
-            </Link>
+          <Button asChild variant="ghost" size="sm">
+            <Link href="/docs/components/chart">Documentation</Link>
           </Button>
-        </PageActions>
+        </PageActions> */}
       </PageHeader>
-      <div className="container py-6">
-        <ExamplesNav className="[&>a:first-child]:text-primary" />
-        <section className="overflow-hidden rounded-lg border bg-background shadow-md md:hidden md:shadow-xl">
-          <Image
-            src="/examples/cards-light.png"
-            width={1280}
-            height={1214}
-            alt="Cards"
-            className="block dark:hidden"
-          />
-          <Image
-            src="/examples/cards-dark.png"
-            width={1280}
-            height={1214}
-            alt="Cards"
-            className="hidden dark:block"
-          />
-        </section>
-        <section className="hidden md:block [&>div]:p-0">
-          <CardsNewYork />
-        </section>
-      </div>
     </div>
   )
 }
