64c4268a3dfad633bfc9df7222159a12eeca233a
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-18 08:41:18 +0000

    feat: colors

--- integration/integration/intregation/material-color-utilities/typescript/index.ts
+++ integration/integration/intregation/material-color-utilities/typescript/index.ts
@@ -53,9 +53,6 @@ export * from './utils/string_utils.js';
 export * from './utils/image_utils.js';
 export * from './utils/theme_utils.js';
 
-// import { argbFromHex, themeFromSourceColor, applyTheme } from "@material/material-color-utilities";
-
-// Get the theme from a hex color
 const theme = themeFromSourceColor(argbFromHex('#f82506'), [
   {
     name: "custom-1",
@@ -64,11 +61,4 @@ const theme = themeFromSourceColor(argbFromHex('#f82506'), [
   },
 ]);
 
-// Print out the theme as JSON
 console.log(JSON.stringify(theme, null, 2));
-
-// // Check if the user has dark mode turned on
-// const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-// // Apply the theme to the body by updating custom properties for material tokens
-// applyTheme(theme, {target: document.body, dark: systemDark});
