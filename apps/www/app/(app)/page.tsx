781d5ff51b4813485bc00ed53539a5d001e8f757
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-20 13:51:35 +0000

    feat: 3 columns for large device homepage or else 2 then for smaller devices 1 column in homepage

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -1,49 +1,115 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
 import { Announcement } from "@/components/announcement"
+import { ExamplesNav } from "@/components/examples-nav"
 import {
   PageActions,
   PageHeader,
   PageHeaderDescription,
   PageHeaderHeading,
 } from "@/components/page-header"
-import { ThemeCustomizer } from "@/components/theme-customizer-1"
-import { ThemeWrapper } from "@/components/theme-wrapper"
 import CardsNewYork from "@/registry/new-york/example/cards"
+import { Button } from "@/registry/new-york/ui/button"
 
-import "public/registry/themes.css"
-
-export default function ThemesPage() {
-
+export default function IndexPage() {
   return (
     <div className="relative">
-      <ThemeWrapper
-        defaultTheme="zinc"
-        className="relative flex w-full flex-col items-start md:flex-row"
-      >
-        <PageHeader className="w-full">
-          <Announcement />
-          <PageHeaderHeading className="hidden md:block">
-            Add colors. Make it yours.
-          </PageHeaderHeading>
-          <PageHeaderHeading className="md:hidden">
-            Make it yours
-          </PageHeaderHeading>
-          <PageHeaderDescription>
-            Hand-picked themes that you can copy and paste into your apps.
-          </PageHeaderDescription>
-          <PageActions>
-            <ThemeCustomizer />
-          </PageActions>
-        </PageHeader>
-      </ThemeWrapper>
+      <PageHeader>
+        <Announcement />
+        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps. Made with Tailwind CSS. Open source.
+        </PageHeaderDescription>
+        <PageActions>
+          <Button asChild size="sm">
+            <Link href="/docs">Get Started</Link>
+          </Button>
+          <Button asChild size="sm" variant="ghost">
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href={siteConfig.links.github}
+            >
+              GitHub
+            </Link>
+          </Button>
+        </PageActions>
+      </PageHeader>
       <div className="container py-6">
-        <section id="themes" className="scroll-mt-20">
-          <div className="space-y-8">
-            <ThemeWrapper>
-              <CardsNewYork />
-            </ThemeWrapper>
-          </div>
+        <ExamplesNav className="[&>a:first-child]:text-primary" />
+        <section className="overflow-hidden rounded-lg border bg-background shadow-md md:hidden md:shadow-xl">
+          <Image
+            src="/examples/cards-light.png"
+            width={1280}
+            height={1214}
+            alt="Cards"
+            className="block dark:hidden"
+          />
+          <Image
+            src="/examples/cards-dark.png"
+            width={1280}
+            height={1214}
+            alt="Cards"
+            className="hidden dark:block"
+          />
+        </section>
+        <section className="hidden md:block [&>div]:p-0">
+          <CardsNewYork />
         </section>
       </div>
     </div>
   )
 }
+
+// import { Announcement } from "@/components/announcement"
+// import {
+//   PageActions,
+//   PageHeader,
+//   PageHeaderDescription,
+//   PageHeaderHeading,
+// } from "@/components/page-header"
+// import { ThemeCustomizer } from "@/components/theme-customizer-1"
+// import { ThemeWrapper } from "@/components/theme-wrapper"
+// import CardsNewYork from "@/registry/new-york/example/cards"
+
+// import "public/registry/themes.css"
+
+// export default function ThemesPage() {
+
+//   return (
+//     <div className="relative">
+//       <ThemeWrapper
+//         defaultTheme="zinc"
+//         className="relative flex w-full flex-col items-start md:flex-row"
+//       >
+//         <PageHeader className="w-full">
+//           <Announcement />
+//           <PageHeaderHeading className="hidden md:block">
+//             Add colors. Make it yours.
+//           </PageHeaderHeading>
+//           <PageHeaderHeading className="md:hidden">
+//             Make it yours
+//           </PageHeaderHeading>
+//           <PageHeaderDescription>
+//             Hand-picked themes that you can copy and paste into your apps.
+//           </PageHeaderDescription>
+//           <PageActions>
+//             <ThemeCustomizer />
+//           </PageActions>
+//         </PageHeader>
+//       </ThemeWrapper>
+//       <div className="container py-6">
+//         <section id="themes" className="scroll-mt-20">
+//           <div className="space-y-8">
+//             <ThemeWrapper>
+//               <CardsNewYork />
+//             </ThemeWrapper>
+//           </div>
+//         </section>
+//       </div>
+//     </div>
+//   )
+// }
