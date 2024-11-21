80090ba1be3252243d02d3850bd60e36bcd28f0d
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-21 14:34:01 +0000

    feat: will add number input component

--- apps/www/config/docs.ts
+++ apps/www/config/docs.ts
@@ -201,7 +201,11 @@ export const docsConfig: DocsConfig = {
           href: "/docs/components/password-input",
           items: [],
         },
-
+        {
+          title: "Number Input",
+          href: "/docs/components/number-input",
+          items: [],
+        },
         {
           title: "Tags Input",
           href: "/docs/components/tags-input",
@@ -222,216 +226,211 @@ export const docsConfig: DocsConfig = {
           href: "/docs/components/buttons",
           items: [],
         },
-        {
-          title: "Background",
-          href: "/docs/components/background",
-          items: [],
-        },
-        {
-          title: "Card",
-          href: "/docs/components/card",
-          items: [],
-        },
-        {
-          title: "Tablist",
-          href: "/docs/components/tablist",
-          items: [],
-        },
-        {
-          title: "Video",
-          href: "/docs/components/video",
-          items: [],
-        },
-        {
-          title: "Audio",
-          href: "/docs/components/audio",
-          items: [],
-        },
-        {
-          title: "3d",
-          href: "/docs/components/3d",
-          items: [],
-        },
-        {
-          title: "AR",
-          href: "/docs/components/ar",
-          items: [],
-        },
-        {
-          title: "VR",
-          href: "/docs/components/vr",
-          items: [],
-        },
-        {
-          title: "Color Picker",
-          href: "/docs/components/color-picker",
-          items: [],
-        },
-        {
-          title: "Notification",
-          href: "/docs/components/notification",
-          items: [],
-        },
-        {
-          title: "Navbar",
-          href: "/docs/components/navbar",
-          items: [],
-        },
-        {
-          title: "Bottombar",
-          href: "/docs/components/bottombar",
-          items: [],
-        },
-        {
-          title: "Footer",
-          href: "/docs/components/footer",
-          items: [],
-        },
-        {
-          title: "Speed Dial",
-          href: "/docs/components/speed-dial",
-          items: [],
-        },
-        {
-          title: "Language Picker",
-          href: "/docs/components/language-picker",
-          items: [],
-        },
-        {
-          title: "Emoji Picker",
-          href: "/docs/components/emoji-picker",
-          items: [],
-        },
-        {
-          title: "GIF Picker",
-          href: "/docs/components/gif-picker",
-          items: [],
-        },
-        {
-          title: "Popdelete",
-          href: "/docs/components/popdelete",
-          items: [],
-        },
-        {
-          title: "Double Popover",
-          href: "/docs/components/double-popover",
-          items: [],
-        },
-        {
-          title: "Two Step Popover",
-          href: "/docs/components/two-step-popover",
-          items: [],
-        },
-        {
-          title: "Tooltip",
-          href: "/docs/components/tooltip",
-          items: [],
-        },
-        {
-          title: "Searchbar",
-          href: "/docs/components/searchbar",
-          items: [],
-        },
-        {
-          title: "Stage Manager",
-          href: "/docs/components/stage-manager",
-          items: [],
-        },
-        {
-          title: "Dynamic Island",
-          href: "/docs/components/dynamic-island",
-          items: [],
-        },
-        {
-          title: "Statusbar",
-          href: "/docs/components/statusbar",
-          items: [],
-        },
-        {
-          title: "Image",
-          href: "/docs/components/image",
-          items: [],
-        },
-        {
-          title: "List",
-          href: "/docs/components/list",
-          items: [],
-        },
-        {
-          title: "Tour",
-          href: "/docs/components/tour",
-          items: [],
-        },
-        {
-          title: "Statics",
-          href: "/docs/components/statics",
-          items: [],
-        },
-        {
-          title: "Timeline",
-          href: "/docs/components/timeline",
-          items: [],
-        },
-        {
-          title: "Description",
-          href: "/docs/components/description",
-          items: [],
-        },
-        {
-          title: "Treeselect",
-          href: "/docs/components/treeselect",
-          items: [],
-        },
-        {
-          title: "Number Input",
-          href: "/docs/components/number-input",
-          items: [],
-        },
-        {
-          title: "Mention",
-          href: "/docs/components/mention",
-          items: [],
-        },
-        {
-          title: "Transfer",
-          href: "/docs/components/transfer",
-          items: [],
-        },
-        {
-          title: "Cascader",
-          href: "/docs/components/cascader",
-          items: [],
-        },
-        {
-          title: "Autocomplete",
-          href: "/docs/components/autocomplete",
-          items: [],
-        },
-        {
-          title: "Steps",
-          href: "/docs/components/steps",
-          items: [],
-        },
-        {
-          title: "Anchor",
-          href: "/docs/components/Anchor",
-          items: [],
-        },
-        {
-          title: "DND",
-          href: "/docs/components/dnd",
-          items: [],
-        },
-        {
-          title: "Particles",
-          href: "/docs/components/particles",
-          items: [],
-        },
-        {
-          title: "Power Mode",
-          href: "/docs/components/power-mode",
-          items: [],
-        },
+        // {
+        //   title: "Background",
+        //   href: "/docs/components/background",
+        //   items: [],
+        // },
+        // {
+        //   title: "Card",
+        //   href: "/docs/components/card",
+        //   items: [],
+        // },
+        // {
+        //   title: "Tablist",
+        //   href: "/docs/components/tablist",
+        //   items: [],
+        // },
+        // {
+        //   title: "Video",
+        //   href: "/docs/components/video",
+        //   items: [],
+        // },
+        // {
+        //   title: "Audio",
+        //   href: "/docs/components/audio",
+        //   items: [],
+        // },
+        // {
+        //   title: "3d",
+        //   href: "/docs/components/3d",
+        //   items: [],
+        // },
+        // {
+        //   title: "AR",
+        //   href: "/docs/components/ar",
+        //   items: [],
+        // },
+        // {
+        //   title: "VR",
+        //   href: "/docs/components/vr",
+        //   items: [],
+        // },
+        // {
+        //   title: "Color Picker",
+        //   href: "/docs/components/color-picker",
+        //   items: [],
+        // },
+        // {
+        //   title: "Notification",
+        //   href: "/docs/components/notification",
+        //   items: [],
+        // },
+        // {
+        //   title: "Navbar",
+        //   href: "/docs/components/navbar",
+        //   items: [],
+        // },
+        // {
+        //   title: "Bottombar",
+        //   href: "/docs/components/bottombar",
+        //   items: [],
+        // },
+        // {
+        //   title: "Footer",
+        //   href: "/docs/components/footer",
+        //   items: [],
+        // },
+        // {
+        //   title: "Speed Dial",
+        //   href: "/docs/components/speed-dial",
+        //   items: [],
+        // },
+        // {
+        //   title: "Language Picker",
+        //   href: "/docs/components/language-picker",
+        //   items: [],
+        // },
+        // {
+        //   title: "Emoji Picker",
+        //   href: "/docs/components/emoji-picker",
+        //   items: [],
+        // },
+        // {
+        //   title: "GIF Picker",
+        //   href: "/docs/components/gif-picker",
+        //   items: [],
+        // },
+        // {
+        //   title: "Popdelete",
+        //   href: "/docs/components/popdelete",
+        //   items: [],
+        // },
+        // {
+        //   title: "Double Popover",
+        //   href: "/docs/components/double-popover",
+        //   items: [],
+        // },
+        // {
+        //   title: "Two Step Popover",
+        //   href: "/docs/components/two-step-popover",
+        //   items: [],
+        // },
+        // {
+        //   title: "Tooltip",
+        //   href: "/docs/components/tooltip",
+        //   items: [],
+        // },
+        // {
+        //   title: "Searchbar",
+        //   href: "/docs/components/searchbar",
+        //   items: [],
+        // },
+        // {
+        //   title: "Stage Manager",
+        //   href: "/docs/components/stage-manager",
+        //   items: [],
+        // },
+        // {
+        //   title: "Dynamic Island",
+        //   href: "/docs/components/dynamic-island",
+        //   items: [],
+        // },
+        // {
+        //   title: "Statusbar",
+        //   href: "/docs/components/statusbar",
+        //   items: [],
+        // },
+        // {
+        //   title: "Image",
+        //   href: "/docs/components/image",
+        //   items: [],
+        // },
+        // {
+        //   title: "List",
+        //   href: "/docs/components/list",
+        //   items: [],
+        // },
+        // {
+        //   title: "Tour",
+        //   href: "/docs/components/tour",
+        //   items: [],
+        // },
+        // {
+        //   title: "Statics",
+        //   href: "/docs/components/statics",
+        //   items: [],
+        // },
+        // {
+        //   title: "Timeline",
+        //   href: "/docs/components/timeline",
+        //   items: [],
+        // },
+        // {
+        //   title: "Description",
+        //   href: "/docs/components/description",
+        //   items: [],
+        // },
+        // {
+        //   title: "Treeselect",
+        //   href: "/docs/components/treeselect",
+        //   items: [],
+        // },
+        // {
+        //   title: "Mention",
+        //   href: "/docs/components/mention",
+        //   items: [],
+        // },
+        // {
+        //   title: "Transfer",
+        //   href: "/docs/components/transfer",
+        //   items: [],
+        // },
+        // {
+        //   title: "Cascader",
+        //   href: "/docs/components/cascader",
+        //   items: [],
+        // },
+        // {
+        //   title: "Autocomplete",
+        //   href: "/docs/components/autocomplete",
+        //   items: [],
+        // },
+        // {
+        //   title: "Steps",
+        //   href: "/docs/components/steps",
+        //   items: [],
+        // },
+        // {
+        //   title: "Anchor",
+        //   href: "/docs/components/Anchor",
+        //   items: [],
+        // },
+        // {
+        //   title: "DND",
+        //   href: "/docs/components/dnd",
+        //   items: [],
+        // },
+        // {
+        //   title: "Particles",
+        //   href: "/docs/components/particles",
+        //   items: [],
+        // },
+        // {
+        //   title: "Power Mode",
+        //   href: "/docs/components/power-mode",
+        //   items: [],
+        // },
 
         {
           title: "Animated Number",
@@ -545,22 +544,17 @@ export const docsConfig: DocsConfig = {
           items: [],
         },
         {
-          title: "MacOS Dock",
-          href: "/docs/components/dock",
-          items: [],
-        },
-        {
           title: "Fractal Grid",
           href: "/docs/components/bg-animated-fractal-grid",
           items: [],
           label: "new",
         },
-        {
-          title: "Shader Lens Blur",
-          href: "/docs/components/shader-lens-blur",
-          items: [],
-          label: "new",
-        },
+        // {
+        //   title: "Shader Lens Blur",
+        //   href: "/docs/components/shader-lens-blur",
+        //   items: [],
+        //   label: "new",
+        // },
         {
           title: "Tweet Grid",
           href: "/docs/components/tweet-grid",
