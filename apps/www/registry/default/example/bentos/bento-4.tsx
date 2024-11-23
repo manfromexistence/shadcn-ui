5cec6e34cbfb6c654efa2187fcb8e91575e2b14f
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-23 17:17:47 +0000

    feat: layout renamed to bentos inside default registry

---
+++ apps/www/registry/default/example/bentos/bento-4.tsx
@@ -0,0 +1,40 @@
+import {
+  Bento_4_v1,
+  Bento_4_v2,
+  Bento_4_v3,
+  Bento_4_v4,
+} from '@/registry/default/ui/bentos/bento-4';
+
+export function Bento_4_v1_Demo() {
+  return (
+    <div className="flex items-center space-x-2">
+      <Bento_4_v1 />
+    </div>
+  )
+}
+
+export function Bento_4_v2_Demo() {
+  return (
+    <div className="flex items-center space-x-2">
+      <Bento_4_v2 />
+    </div>
+  )
+}
+
+export function Bento_4_v3_Demo() {
+  return (
+    <div className="flex items-center space-x-2">
+      <Bento_4_v3 />
+    </div>
+  )
+}
+
+export function Bento_4_v4_Demo() {
+  return (
+    <div className="flex items-center space-x-2">
+      <Bento_4_v4 />
+    </div>
+  )
+}
+
+
