81e6bde880fd5af3022afaf0275641df007c2654
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-29 02:50:50 +0000

    feat: will new charts

--- apps/www/visualizations/translate.js
+++ apps/www/visualizations/translate.js
@@ -4,30 +4,6 @@ const path = require('path');
 
 translator.supportedLanguages();
 
-// async function translateJson(filePath) {
-//     const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
-//     for (const demo of jsonData.demos) {
-//         translator
-//             .TranslateLanguageData({
-//                 listOfWordsToTranslate: [demo.title],
-//                 fromLanguage: "zh-CN",
-//                 toLanguage: "en",
-//             })
-//             .then((data) => {
-//                 console.log(data);
-//                 demo.title = data;
-//                 fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
-//             });
-//     }
-
-//     console.log('Translation completed!');
-// }
-
-// translateJson('column/stack/demo/meta.json');
-
-
-
 function findMetaJsonFiles(dir) {
     const files = fs.readdirSync(dir, { withFileTypes: true });
 
