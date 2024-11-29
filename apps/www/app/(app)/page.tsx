3056c23835b068b1ecb19fc61dc003f4938e6651
manfromexistence <ajju40959@gmail.com>
2024-11-29 16:31:09 +0000

    feat: diagrams

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -37,6 +37,7 @@ import TabsDemo from "@/antd/tabs"
 import TreeDemo from "@/antd/tree"
 import FloatButtonDemo from "@/antd/float-button"
 import Charts from "@/visualizations/charts/area/area/demo/area"
+import Diagrams from "@/visualizations/diagrams/edge/connector/demo/diagrams"
 
 export default function IndexPage() {
 
@@ -74,6 +75,32 @@ export default function IndexPage() {
             // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
           }}>
           {/* <Charts /> */}
+          <Diagrams
+            options={{
+              container: 'your-container-id', // Or a DOM element reference
+              width: 500,
+              height: 500,
+              data: {
+                nodes: [
+                  {
+                    id: 'node-1',
+                    style: { x: 50, y: 100 },
+                  },
+                  {
+                    id: 'node-2',
+                    style: { x: 150, y: 100 },
+                  },
+                ],
+                edges: [{ id: 'edge-1', source: 'node-1', target: 'node-2' }],
+              },
+            }}
+            onRender={() => {
+              // Do something after the graph is rendered
+            }}
+            onDestroy={() => {
+              // Do something before the graph is destroyed
+            }}
+          />
         </ConfigProvider>
       </div>
 
