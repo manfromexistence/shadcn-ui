a6d5f46e41dc755a70f00afb328701f9c207ffca
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-11-29 01:45:00 +0000

    feat: translate

--- apps/www/visualizations/charts/mobile/translate.js
+++ apps/www/visualizations/charts/mobile/translate.js
@@ -1,26 +1,60 @@
 const fs = require('fs');
 const translator = require("@/ogt/src");
+const path = require('path');
 
 translator.supportedLanguages();
 
-async function translateJson(filePath) {
-    const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+// async function translateJson(filePath) {
+//     const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-    for (const demo of jsonData.demos) {
-        translator
-            .TranslateLanguageData({
-                listOfWordsToTranslate: [demo.title],
-                fromLanguage: "zh-CN",
-                toLanguage: "en",
-            })
-            .then((data) => {
-                console.log(data);
-                demo.title = data;
-                fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
-            });
+//     for (const demo of jsonData.demos) {
+//         translator
+//             .TranslateLanguageData({
+//                 listOfWordsToTranslate: [demo.title],
+//                 fromLanguage: "zh-CN",
+//                 toLanguage: "en",
+//             })
+//             .then((data) => {
+//                 console.log(data);
+//                 demo.title = data;
+//                 fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+//             });
+//     }
+
+//     console.log('Translation completed!');
+// }
+
+// translateJson('column/stack/demo/meta.json');
+
+
+
+function findMetaJsonFiles(dir) {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+
+    for (const file of files) {
+        const filePath = path.join(dir, file.name);
+
+        if (file.isDirectory()) {
+            findMetaJsonFiles(filePath);
+        } else if (file.name === 'meta.json') {
+            console.log(filePath);
+            const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+            for (const demo of jsonData.demos) {
+                translator
+                    .TranslateLanguageData({
+                        listOfWordsToTranslate: [demo.title],
+                        fromLanguage: "zh-CN",
+                        toLanguage: "en",
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        demo.title = data;
+                        fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+                    });
+            }
+        }
     }
-
-    console.log('Translation completed!');
 }
 
-translateJson('column/stack/demo/meta.json');
+const rootDir = './';
+findMetaJsonFiles(rootDir);
