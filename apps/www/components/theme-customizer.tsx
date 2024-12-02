cbf8542545c472144fd17367e87ac1320261c63c
manfromexistence <ajju40959@gmail.com>
2024-12-02 04:05:08 +0000

    feat: making config in themes page

--- apps/www/components/theme-customizer.tsx
+++ apps/www/components/theme-customizer.tsx
@@ -74,6 +74,29 @@ export function ThemeCustomizer() {
         </Popover>
       </div>
       {/* <CopyCodeButton variant="ghost" size="sm" className="[&_svg]:hidden" /> */}
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button variant={"outline"} size="sm" className="md:hidden">
+            Config
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className="p-6 pt-0">
+          <Config />
+        </DrawerContent>
+      </Drawer>
+      <div className="hidden items-center md:flex">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant={"outline"} size="sm">Config</Button>
+          </PopoverTrigger>
+          <PopoverContent
+            align="start"
+            className="z-40 w-[340px] rounded-[12px] bg-white p-6 dark:bg-zinc-950"
+          >
+            <Config />
+          </PopoverContent>
+        </Popover>
+      </div>
     </div>
   )
 }
@@ -98,7 +121,7 @@ function Customizer() {
             Theme Customizer
           </div>
           <div className="text-xs text-muted-foreground">
-            Customize your components colors.
+            Customize your components.
           </div>
         </div>
         <Button
@@ -226,6 +249,159 @@ function Customizer() {
           </div>
         </div>
       </div>
+    </ThemeWrapper>
+  )
+}
+
+function Config() {
+  const [mounted, setMounted] = React.useState(false)
+  const { setTheme: setMode, resolvedTheme: mode } = useTheme()
+  const [config, setConfig] = useConfig()
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <ThemeWrapper
+      defaultTheme="zinc"
+      className="flex flex-col space-y-4 md:space-y-6"
+    >
+      <div className="flex items-start pt-4 md:pt-0">
+        <div className="space-y-1 pr-2">
+          <div className="font-semibold leading-none tracking-tight">
+            Config
+          </div>
+          <div className="text-xs text-muted-foreground">
+            Import of Export your config.
+          </div>
+        </div>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="ml-auto rounded-[0.5rem]"
+          onClick={() => {
+            setConfig({
+              ...config,
+              theme: "zinc",
+              radius: 0.5,
+            })
+          }}
+        >
+          <Repeat />
+          <span className="sr-only">Reset</span>
+        </Button>
+        {/* <CopyCodeButton variant="ghost" size="sm" /> */}
+      </div>
+      {/* <div className="flex flex-1 flex-col space-y-4 md:space-y-6">
+        <div className="space-y-1.5">
+          <Label className="text-xs">Color</Label>
+          <div className="grid grid-cols-3 gap-2">
+            {baseColors
+              .filter(
+                (theme) =>
+                  !["slate", "stone", "gray", "neutral"].includes(theme.name)
+              )
+              .map((theme) => {
+                const isActive = config.theme === theme.name
+
+                return mounted ? (
+                  <Button
+                    variant={"outline"}
+                    size="sm"
+                    key={theme.name}
+                    onClick={() => {
+                      setConfig({
+                        ...config,
+                        theme: theme.name,
+                      })
+                    }}
+                    className={cn(
+                      "justify-start",
+                      isActive && "border-2 border-primary"
+                    )}
+                    style={
+                      {
+                        "--theme-primary": `hsl(${theme?.activeColor[mode === "dark" ? "dark" : "light"]
+                          })`,
+                      } as React.CSSProperties
+                    }
+                  >
+                    <span
+                      className={cn(
+                        "mr-1 flex h-5 w-5 shrink-0 -translate-x-1 items-center justify-center rounded-full bg-[--theme-primary]"
+                      )}
+                    >
+                      {isActive && <Check className="h-4 w-4 text-white" />}
+                    </span>
+                    {theme.label}
+                  </Button>
+                ) : (
+                  <Skeleton className="h-8 w-full" key={theme.name} />
+                )
+              })}
+          </div>
+        </div>
+        <div className="space-y-1.5">
+          <Label className="text-xs">Radius</Label>
+          <div className="grid grid-cols-5 gap-2">
+            {["0", "0.3", "0.5", "0.75", "1.0"].map((value) => {
+              return (
+                <Button
+                  variant={"outline"}
+                  size="sm"
+                  key={value}
+                  onClick={() => {
+                    setConfig({
+                      ...config,
+                      radius: parseFloat(value),
+                    })
+                  }}
+                  className={cn(
+                    config.radius === parseFloat(value) &&
+                    "border-2 border-primary"
+                  )}
+                >
+                  {value}
+                </Button>
+              )
+            })}
+          </div>
+        </div>
+        <div className="space-y-1.5">
+          <Label className="text-xs">Mode</Label>
+          <div className="grid grid-cols-3 gap-2">
+            {mounted ? (
+              <>
+                <Button
+                  variant={"outline"}
+                  size="sm"
+                  onClick={() => setMode("light")}
+                  className={cn(mode === "light" && "border-2 border-primary")}
+                >
+                  <Sun className="mr-1 -translate-x-1" />
+                  Light
+                </Button>
+                <Button
+                  variant={"outline"}
+                  size="sm"
+                  onClick={() => setMode("dark")}
+                  className={cn(mode === "dark" && "border-2 border-primary")}
+                >
+                  <Moon className="mr-1 -translate-x-1" />
+                  Dark
+                </Button>
+              </>
+            ) : (
+              <>
+                <Skeleton className="h-8 w-full" />
+                <Skeleton className="h-8 w-full" />
+              </>
+            )}
+          </div>
+        </div>
+      </div> */}
+      
     </ThemeWrapper>
   )
 }
