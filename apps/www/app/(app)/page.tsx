c5fea371144382f886eeb5be11d7a20917ba7ed5
manfromexistence <ajju40959@gmail.com>
2024-12-15 06:14:02 +0000

    feat: hello

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -58,164 +58,169 @@ export default function IndexPage() {
           </Button>
         </PageActions>
       </PageHeader>
-      <div className="grid w-full sm:grid-cols-1 md:grid-cols-2 lg:h-[500px] lg:grid-cols-3">
-        <Link href="/docs/components/dock" className="flex h-full w-full flex-col items-start justify-between border-b border-r p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary">
-          <div className="h-24 w-full space-y-2">
-            <div className="flex items-center space-x-2">
-              <div className="flex w-full items-center justify-between rounded-md border bg-primary-foreground p-3">
-                <span className="truncate text-sm md:w-32 lg:w-24 xl:w-48 2xl:w-full">$ npx manfromexistence-ui init</span>
-                <Copy className="h-4 w-4" />
-              </div>
-              <div className="flex w-32 flex-1 items-center justify-center rounded-md border bg-primary-foreground p-3 text-sm">
-                <UserPlus className="mr-2 h-4 w-4" />
-                Collabrators
-              </div>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div className="flex rounded-md border bg-primary-foreground p-3">
-                <ShieldEllipsis className="h-5 w-5" />
-              </div>
-              <div className="flex rounded-md border p-1.5">
-                <div className="flex items-center justify-center rounded-md border bg-secondary p-1.5">
-                  <LayoutGrid className="h-5 w-5" />
+      <div className="w-full">
+        <div className="grid w-full sm:grid-cols-1 md:grid-cols-2 lg:h-[500px] lg:grid-cols-3">
+
+          <Link href="/docs/components/dock" className="flex h-full w-full flex-col items-start justify-between border-b border-r p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary">
+            <div className="h-24 w-full space-y-2">
+              <div className="flex items-center space-x-2">
+                <div className="flex w-full items-center justify-between rounded-md border bg-primary-foreground p-3">
+                  <span className="truncate text-sm md:w-32 lg:w-24 xl:w-48 2xl:w-full">$ npx manfromexistence-ui init</span>
+                  <Copy className="h-4 w-4" />
                 </div>
-                <div className="flex items-center justify-center rounded-md p-1.5">
-                  <List className="h-5 w-5" />
+                <div className="flex w-32 flex-1 items-center justify-center rounded-md border bg-primary-foreground p-3 text-sm">
+                  <UserPlus className="mr-2 h-4 w-4" />
+                  Collabrators
                 </div>
               </div>
-              <div className="flex h-12 w-full rounded-md border bg-primary-foreground text-sm">
-                <span className="flex h-full items-center justify-center rounded-l-md bg-secondary px-3">
-                  Label
-                </span>
-                <span className="flex h-full w-full flex-1 items-center border-l p-3">
-                  Value
-                </span>
+              <div className="flex items-center space-x-2">
+                <div className="flex rounded-md border bg-primary-foreground p-3">
+                  <ShieldEllipsis className="h-5 w-5" />
+                </div>
+                <div className="flex rounded-md border p-1.5">
+                  <div className="flex items-center justify-center rounded-md border bg-secondary p-1.5">
+                    <LayoutGrid className="h-5 w-5" />
+                  </div>
+                  <div className="flex items-center justify-center rounded-md p-1.5">
+                    <List className="h-5 w-5" />
+                  </div>
+                </div>
+                <div className="flex h-12 w-full rounded-md border bg-primary-foreground text-sm">
+                  <span className="flex h-full items-center justify-center rounded-l-md bg-secondary px-3">
+                    Label
+                  </span>
+                  <span className="flex h-full w-full flex-1 items-center border-l p-3">
+                    Value
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="mt-6 md:mt-4 lg:mt-0">
-            <h1 className="text-primary">Components</h1>
-            <span className="text-muted-foreground">Building Blocks for your software.</span>
-          </div>
-        </Link>
-        <Link href="/icons" className="flex h-full w-full flex-col justify-between border-b border-r p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary">
-          <div className="grid h-24 w-full grid-cols-8 grid-rows-3 place-content-center items-center justify-center gap-8">
-            <PartyPopper />
-            <PawPrint />
-            <AlarmClock />
-            <Apple />
-            <Axe />
-            <Bell />
-            <Beer />
-            <BookOpenText />
-            <Braces />
-            <Box />
-            <BookMarked />
-            <Brush />
-            <Bug />
-            <Calendar />
-            <Cable />
-            <Carrot />
-            <CircleDashed />
-            <Citrus />
-            <Clapperboard />
-            <CircleSlash2 />
-            <CloudSnow />
-            <Cog />
-            <Command />
-            <CodeXml />
-          </div>
-          <div className="mt-6 md:mt-4 lg:mt-0">
-            <h1 className="text-primary">Icons</h1>
-            <span className="text-muted-foreground">Icons for everyone.</span>
-          </div>
-        </Link>
-        <Link href="/animations" className="flex h-full w-full  flex-col justify-between border-b p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary md:border-r">
-          <div className="flex h-24 w-full justify-between space-x-1">
-            <div className="h-24 w-24 rounded-full border bg-secondary"></div>
-            <div className="h-24 w-24 rounded-full border bg-secondary"></div>
-            <div className="h-24 w-24 rounded-full border bg-secondary"></div>
-            <div className="h-24 w-24 rounded-full border bg-secondary md:hidden 2xl:block"></div>
-            <div className="h-24 w-24 rounded-full border bg-secondary lg:hidden"></div>
-          </div>
-          <div className="mt-6 md:mt-4 lg:mt-0">
-            <h1 className="text-primary">Animations</h1>
-            <span className="text-muted-foreground">Animations for everyone.</span>
-          </div>
-        </Link>
-        <Link href="/docs/systems/grid" className="flex h-full w-full flex-col items-start justify-between border-b p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary md:border-r-0 lg:border-b-0 lg:border-r">
-          <div className="relative grid h-24 w-full grid-cols-9 border">
-            <PlusIcon className="absolute -left-5 -top-5 h-10 w-10" />
-            <PlusIcon className="absolute -bottom-5 -right-5 h-10 w-10" />
+            <div className="mt-6 md:mt-4 lg:mt-0">
+              <h1 className="text-primary">Components</h1>
+              <span className="text-muted-foreground">Building Blocks for your software.</span>
+            </div>
+          </Link>
+          <Link href="/icons" className="flex h-full w-full flex-col justify-between border-b border-r p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary">
+            <div className="grid h-24 w-full grid-cols-8 grid-rows-3 place-content-center items-center justify-center gap-8">
+              <PartyPopper />
+              <PawPrint />
+              <AlarmClock />
+              <Apple />
+              <Axe />
+              <Bell />
+              <Beer />
+              <BookOpenText />
+              <Braces />
+              <Box />
+              <BookMarked />
+              <Brush />
+              <Bug />
+              <Calendar />
+              <Cable />
+              <Carrot />
+              <CircleDashed />
+              <Citrus />
+              <Clapperboard />
+              <CircleSlash2 />
+              <CloudSnow />
+              <Cog />
+              <Command />
+              <CodeXml />
+            </div>
+            <div className="mt-6 md:mt-4 lg:mt-0">
+              <h1 className="text-primary">Icons</h1>
+              <span className="text-muted-foreground">Icons for everyone.</span>
+            </div>
+          </Link>
+          <Link href="/animations" className="flex h-full w-full  flex-col justify-between border-b p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary md:border-r">
+            <div className="flex h-24 w-full justify-between space-x-1">
+              <div className="h-24 w-24 rounded-full border bg-secondary"></div>
+              <div className="h-24 w-24 rounded-full border bg-secondary"></div>
+              <div className="h-24 w-24 rounded-full border bg-secondary"></div>
+              <div className="h-24 w-24 rounded-full border bg-secondary md:hidden 2xl:block"></div>
+              <div className="h-24 w-24 rounded-full border bg-secondary lg:hidden"></div>
+            </div>
+            <div className="mt-6 md:mt-4 lg:mt-0">
+              <h1 className="text-primary">Animations</h1>
+              <span className="text-muted-foreground">Animations for everyone.</span>
+            </div>
+          </Link>
+          <Link href="/docs/systems/grid" className="flex h-full w-full flex-col items-start justify-between border-b p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary md:border-r-0 lg:border-b-0 lg:border-r">
+            <div className="relative grid h-24 w-full grid-cols-9 border">
+              <PlusIcon className="absolute -left-5 -top-5 h-10 w-10" />
+              <PlusIcon className="absolute -bottom-5 -right-5 h-10 w-10" />
 
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b border-r"></span>
-            <span className="h-full w-full border-b"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b border-r"></span>
+              <span className="h-full w-full border-b"></span>
 
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full border-r"></span>
-            <span className="h-full w-full"></span>
-          </div>
-          <div className="mt-6 md:mt-4 lg:mt-0">
-            <h1 className="text-primary">Systems</h1>
-            <span className="text-muted-foreground">Make it yours.</span>
-          </div>
-        </Link>
-        <Link href="/fonts" className="flex h-full w-full flex-col justify-between border-b border-r p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary md:border-b-0">
-          <div className="flex h-24 w-full rounded-md border border-dashed">
-            <span className="flex h-full w-full items-center justify-center border-r border-dashed text-2xl font-bold italic">Fonts Sans</span>
-            <span className="flex h-full w-full items-center justify-center text-2xl font-bold">Fonts Mono</span>
-          </div>
-          <div className="mt-6 md:mt-4 lg:mt-0">
-            <h1 className="text-primary">Fonts</h1>
-            <span className="text-muted-foreground">Fonts for everyone.</span>
-          </div>
-        </Link>
-        <Link href="/colors" className="flex h-full w-full flex-col justify-between p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary">
-          <div className="flex h-24 w-full justify-between">
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-red-500" />
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full border-r"></span>
+              <span className="h-full w-full"></span>
             </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-green-500" />
+            <div className="mt-6 md:mt-4 lg:mt-0">
+              <h1 className="text-primary">Systems</h1>
+              <span className="text-muted-foreground">Make it yours.</span>
             </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-yellow-500" />
+          </Link>
+          <Link href="/fonts" className="flex h-full w-full flex-col justify-between border-b border-r p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary md:border-b-0">
+            <div className="flex h-24 w-full rounded-md border border-dashed">
+              <span className="flex h-full w-full items-center justify-center border-r border-dashed text-2xl font-bold italic">Fonts Sans</span>
+              <span className="flex h-full w-full items-center justify-center text-2xl font-bold">Fonts Mono</span>
             </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-teal-500" />
+            <div className="mt-6 md:mt-4 lg:mt-0">
+              <h1 className="text-primary">Fonts</h1>
+              <span className="text-muted-foreground">Fonts for everyone.</span>
             </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-purple-500" />
+          </Link>
+          <Link href="/colors" className="flex h-full w-full flex-col justify-between p-9 text-muted-foreground hover:bg-primary-foreground hover:text-primary">
+            <div className="flex h-24 w-full justify-between">
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-red-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-green-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-yellow-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-teal-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-purple-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-fuchsia-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-blue-500" />
+              </div>
+              <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
+                <div className="h-full w-2 rounded-full bg-orange-500" />
+              </div>
             </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-fuchsia-500" />
+            <div className="mt-6 md:mt-4 lg:mt-0">
+              <h1 className="text-primary">Colors</h1>
+              <span className="text-muted-foreground">Colors for everyone.</span>
             </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-blue-500" />
-            </div>
-            <div className="flex h-full w-8 items-center justify-center rounded-full border p-3">
-              <div className="h-full w-2 rounded-full bg-orange-500" />
-            </div>
-          </div>
-          <div className="mt-6 md:mt-4 lg:mt-0">
-            <h1 className="text-primary">Colors</h1>
-            <span className="text-muted-foreground">Colors for everyone.</span>
-          </div>
-        </Link>
+          </Link>
+
+        </div>
       </div>
+
     </div>
   )
 }
