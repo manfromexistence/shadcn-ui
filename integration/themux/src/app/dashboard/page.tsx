87197ccaba90efc2a00f873d1c5ed9ccbf61dd47
manfromexistence <89142466+manfromexistence@users.noreply.github.com>
2025-04-29 15:42:56 +0000

    remove: themux

--- integration/themux/src/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardPage } from "@/components/dashboard-demo";
-
-export default function Page() {
-  return <DashboardPage />;
-}
--- integration/themux/src/app/mail/page.tsx
+++
@@ -1,5 +0,0 @@
-import { MailPage } from "@/components/mail-demo";
-
-export default function Page() {
-  return <MailPage />;
-}
